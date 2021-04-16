9cec7122595f8445fee6c1196286d92b402d621e
adamslou <ladams321@hotmail.com>
2021-04-15 21:00:51 -0500

    Adding Menu and Game scenes

---
+++ Assets/Game.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    public void QuitHunt()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
