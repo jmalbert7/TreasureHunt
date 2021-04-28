9b24b6f1d54b72b37a8bd91ef7d5af060567b5f1
Jessica Albert <jessmalbert0807@gmail.com>
2021-04-27 20:49:53 -0500

    create hunt endpoint

---
+++ AzureFunctions/TreasureHunt.API/TreasureHunt.API/Hunts/CreateHunt.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace TreasureHunt.API.Hunts
+{
+    public static class CreateHunt
+    {
+        [FunctionName("CreateHunt")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous,"post", Route = "hunts/")] HttpRequest req,
+            ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            var userid =  data?.userid;
+            var name = data?.name;
+            var location = data?.location;
+
+            if (userid == null || name == null || location == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent("Usage: userid, name, location needed in request body", Encoding.UTF8, "text/plain")
+                };
+            }
+
+            var connectionString = Environment.GetEnvironmentVariable("sqldb_connection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                //SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY] returns the id of the inserted row
+                var query = $"INSERT Hunts (UserId, HuntName, GeneralLocation) VALUES ('{userid}', '{name}', '{location}')SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY];";
+                try
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        var row = await command.ExecuteScalarAsync();
+                        if (row != null)
+                        {
+                            var res = JsonConvert.SerializeObject(Int32.Parse(row.ToString()));
+                            return new HttpResponseMessage(HttpStatusCode.OK)
+                            {
+                                Content = new StringContent(res, Encoding.UTF8, "application/json")
+                            };
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new HttpResponseMessage(HttpStatusCode.BadRequest) 
+                    { 
+                        Content = new StringContent(ex.Message, Encoding.UTF8, "text/plain")
+                    };
+                }
+            }
+            return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+        }
+    }
+}
+
