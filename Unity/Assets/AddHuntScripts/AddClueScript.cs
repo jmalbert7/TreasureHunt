3fffda747413937b7565a4dac8ffab0f806977af
Logan Cope <cope_logan@gmail.com>
2021-05-10 13:35:52 -0400

    Updated AddClueScript and AddClueMenu

--- Unity/Assets/AddHuntScripts/AddClueScript.cs
+++ Unity/Assets/AddHuntScripts/AddClueScript.cs
@@ -15,12 +15,15 @@ public class AddClueScript : MonoBehaviour
     
 
     readonly string getUrl = "https://functionapplicationgroupx.azurewebsites.net/api/clues/?huntid=";
-    //readonly string getUrl = "https://treasurehuntgroupx.azurewebsites.net/api/hunts/?userid=";
+    readonly string postUrl = "https://functionapplicationgroupx.azurewebsites.net/api/clues/";
+
 
     public string riddle;
     public int firstflag = 1;
-    public int lastflag = 0;
-    public int lastClueId = 0;
+    //public bool firstflag = true;
+    //public int lastflag = 1; 
+    public bool lastflag = true;
+    public int lastClueId = 2;
     private int huntId = 1;     // Using huntId 1 for now 
   
 
@@ -41,6 +44,16 @@ public class AddClueScript : MonoBehaviour
        
     }
 
+    public void IncrementFirstFlag()
+    {
+        firstflag += 1;
+    }
+
+    public void ClearInputField()
+    {
+        riddleInput.text = "";
+    }
+
     public void OnButtonCallAzureFunction()
     {
         StartCoroutine(AzureGetRequest());
@@ -58,17 +71,48 @@ public class AddClueScript : MonoBehaviour
         {
             location = ReverseGeocodeOnClick.latitude.ToString() + "," + ReverseGeocodeOnClick.longitude.ToString();
         }
-
+        string azureUrl;
         string firstFlagParam = "&firstflag=";
         string lastClueIdParam = "&lastclueid=";
         string lastFlagParam = "&lastflag=";
         string locationParam = "&location=";
         string riddleParam = "&riddle=";
 
-        string azureUrl = getUrl + huntId + firstFlagParam + firstflag + lastClueIdParam + lastClueId + lastFlagParam + lastflag + locationParam + location + riddleParam + riddle;
+        // Check if on first clue or not
+        if (firstflag == 1)
+        {
+            azureUrl = getUrl + huntId + locationParam + location + riddleParam + riddle + firstFlagParam + firstflag;
+        }
+        else
+        {
+            azureUrl = getUrl + huntId + locationParam + location + riddleParam + riddle;
+        }
+        
+
         Debug.Log(location);
         Debug.Log(azureUrl);
+        Debug.Log(firstflag);
         UnityWebRequest www = UnityWebRequest.Get(azureUrl);
+    
+
+        //Form to make post request
+        //List<IMultipartFormSection> wwwForm = new List<IMultipartFormSection>();
+        //wwwForm.Add(new MultipartFormDataSection("huntid", "1"));
+       // wwwForm.Add(new MultipartFormDataSection("firstflag", "1"));
+        //wwwForm.Add(new MultipartFormDataSection("lastflag", "0"));
+       // wwwForm.Add(new MultipartFormDataSection("lastclueid", "1"));
+       // wwwForm.Add(new MultipartFormDataSection("location", "coord"));
+        //wwwForm.Add(new MultipartFormDataSection("riddle", "riddle"));
+
+        // Try this form type
+        //WWWForm form = new WWWForm();
+        //form.AddField("huntid", 1);
+        //form.AddField("firstflag", 1);
+        //form.AddField("lastflag", 0);
+        //form.AddField("lastclueid", "1");
+        //form.AddField("location", "coord");
+        //form.AddField("riddle", "riddle");
+        //UnityWebRequest www = UnityWebRequest.Post(postUrl, form);
 
         yield return www.SendWebRequest();
         if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
